582e5c1ec81a37c1959a67241b8392459d6ba3fb
John Batte <john.batte@invivolink.com>
2022-03-09 07:39:46 -0600

    leftover cleanup

--- react-router-test-app/src/components/secret-router-experiment.tsx
+++ react-router-test-app/src/components/secret-router-experiment.tsx
@@ -10,23 +10,21 @@ export const SecretRouterExperiments: FC<INavHooks> = hooks => {
     updateSearchParams,
   } = hooks;
   return (
-    <>
-      <RouterExperiments { ...hooks }>
-        <p>
-          The <strong>Secret Internal ID</strong> value is: <strong>{ someInternalId }</strong>.
-        </p>
-        <p>
-          <InputGroup>
-            <InputGroup.Text>Set an arg value:</InputGroup.Text>
-            <input
-              type="text"
-              aria-label="route"
-              onChange={ ({ currentTarget: { value } }) => updateSearchParams({ programmaticArg: value }) }>
-            </input>
-          </InputGroup>
-        </p>
-      </RouterExperiments>
-    </>
+    <RouterExperiments { ...hooks }>
+      <p>
+        The <strong>Secret Internal ID</strong> value is: <strong>{ someInternalId }</strong>.
+      </p>
+      <p>
+        <InputGroup>
+          <InputGroup.Text>Set an arg value:</InputGroup.Text>
+          <input
+            type="text"
+            aria-label="route"
+            onChange={ ({ currentTarget: { value } }) => updateSearchParams({ programmaticArg: value }) }>
+          </input>
+        </InputGroup>
+      </p>
+    </RouterExperiments>
   );
 };
 
@@ -34,7 +32,7 @@ const WithHooks = () => {
   const navHooks = useNavHooks();
   return (
     <div>
-      { <SecretRouterExperiments { ...navHooks } /> }
+      <SecretRouterExperiments { ...navHooks } />
     </div>
   );
 };
